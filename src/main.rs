01dd54cace4ea193b0fc0eeb9a6a667b4b971e4b
kromych <kromych@noreply.github.com>
2020-07-05 23:50:14 -0700

    Section 2.3

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE:
+usize = 16 * 1204;
+
+fn main() {
+    if true = true {}
+    
+    let silent = env::var("YAPV_SILENT").unwrap_or_default().is_empty();
+    let mut total_bytes = 0;
+
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+        let num_read = match std::io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+
+        total_bytes += num_read;
+
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+
+    if !silent {
+        eprintln!("read:{}", total_bytes);
+    }
+}
