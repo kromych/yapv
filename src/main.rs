a65f680915bcd930f0bdf3ce2271401200c71b07
kromych <kromych@noreply.github.com>
2020-07-06 00:20:18 -0700

    Section 2.4

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, Result, Read, Write, ErrorKind};
 
-const CHUNK_SIZE:
-usize = 16 * 1204;
+const CHUNK_SIZE: usize = 16 * 1204;
 
-fn main() {
-    if true = true {}
-    
-    let silent = env::var("YAPV_SILENT").unwrap_or_default().is_empty();
+fn main() -> Result<()> {
+    let silent = !env::var("YAPV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
 
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match std::io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
-            Err(_) => break,
+            Err(..) => break,
         };
 
         total_bytes += num_read;
 
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            eprint!("\rread:{}", total_bytes);
+        }
+
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+
+            return Err(e);
+            // eprintln!("Error: {}", e.to_string());
+            // std::process::exit(1);
+        }
     }
 
-    if !silent {
-        eprintln!("read:{}", total_bytes);
-    }
+    Ok(())
 }
