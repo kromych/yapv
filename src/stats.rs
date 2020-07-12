b9a815f577ca298166c05b457470d8ad1ce410e9
kromych <kromych@noreply.github.com>
2020-07-11 20:22:05 -0700

    Outputtting rate

--- src/stats.rs
+++ src/stats.rs
@@ -1,16 +1,28 @@
 use std::io::Result;
+use std::time::Instant;
 
 use crossbeam::Receiver;
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
+    let start = Instant::now();
+    let mut last_instant = start;
 
     loop {
         let num_bytes = stats_rx.recv().unwrap();
+        let now = Instant::now();
+        let rate_per_second = num_bytes as f64 / (now - last_instant).as_secs_f64();
+        last_instant = now;
+
         total_bytes += num_bytes;
 
         if !silent {
-            eprint!("\rread:{}", total_bytes);
+            eprint!(
+                "\r{} bytes {} sec [{:.0} b/s]",
+                total_bytes,
+                start.elapsed().as_secs(),
+                rate_per_second
+            );
         }
 
         if num_bytes == 0 {
